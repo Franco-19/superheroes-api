8f7997fce217130e1a2924cb5f66dc106dabca66
Franco Jara <jarafranco122@gmail.com>
2021-11-29 18:48:16 -0300

    adding axios, formik and search list of characters

--- src/components/search/Search.jsx
+++ src/components/search/Search.jsx
@@ -1,10 +1,48 @@
-import React from 'react'
+import React, { Fragment, useState } from 'react'
+
+import { useFormik } from 'formik';
+import axios from 'axios';
+import ListCharacters from '../ListCharacters/ListCharacters';
 
 export default function Search() {
+
+    const [characters, setCharacters] = useState([]);
+
+    const searchCharacter = (name) => {
+        axios.get(`https://www.superheroapi.com/api.php/3085941028317253/search/${name}`)
+            .then((response) => {
+                // console.log(response.data.results)
+                if(response.data.response == 'success'){
+                    setCharacters(response.data.results)
+                }
+            })
+    }
+
+    const formik = useFormik({
+      initialValues: {
+        searchInput: '',
+      },
+      onSubmit: values => {
+        searchCharacter(values.searchInput)
+      },
+    });
+
     return (
-        <form className="d-flex mx-4 ">
-            <input className="form-control me-2" type="search" placeholder="Nombre de heroe" aria-label="Search"/>
-            <button className="btn btn-outline-success" type="submit">Buscar</button>
+      <Fragment>
+        <form onSubmit={formik.handleSubmit} className="d-flex mx-4">
+          <input
+              className="form-control me-2"
+              id="searchInput"
+              name="searchInput"
+              type="text"
+              onChange={formik.handleChange}
+              value={formik.values.searchInput}
+          />
+        
+          {/* <button type="submit">Submit</button> */}
+          <button className="btn btn-outline-success" type="submit" >Buscar</button>
         </form>
-    )
+        <ListCharacters searchResultArray={characters} />
+      </Fragment>
+    );
 }
--- src/pages/CreateTeam.jsx
+++ src/pages/CreateTeam.jsx
@@ -4,7 +4,7 @@ import Search from '../components/search/Search'
 export default function CreateTeam() {
     return (
         <div>
-            <p>Aqui crearemos nuestro primer equipo!</p>
+            <p>¡Aqui crearemos nuestro primer equipo! <br /> Utiliza el buscador para encontrar tu personaje favorito</p>
             <Search/>
         </div>
     )
